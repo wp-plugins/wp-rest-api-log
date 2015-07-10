a341e8a05b2a574f45252cbf1004bda1d2928334
GunGeekATX <GunGeekATX@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-10 00:44:34 +0000

    Initial beta release
    git-svn-id: https://plugins.svn.wordpress.org/wp-rest-api-log/tags/1.0.0-beta1@1195933 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/class-wp-rest-api-log-admin.php
@@ -0,0 +1,103 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) die( 'restricted access' );
+
+if ( ! class_exists( 'WP_REST_API_Log_Admin' ) ) {
+
+	class WP_REST_API_Log_Admin {
+
+
+		public function plugins_loaded() {
+			add_action( 'admin_init', array( $this, 'admin_init' ) );
+			add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		}
+
+
+		public function admin_init() {
+
+		}
+
+
+		public function admin_menu() {
+			add_submenu_page( 'tools.php', 'WP REST API Log', 'WP REST API Log', 'manage_options', WP_REST_API_Log_Common::$plugin_name, array( $this, 'display_entries' ) );
+		}
+
+
+		public function display_entries() {
+
+			if ( ! WP_REST_API_Log_Common::api_is_enabled() ) {
+				require_once dirname( __FILE__ ) . '/partials/wp-rest-api-log-api-is-disabled.php';
+				return;
+			}
+
+
+			global $wp_rest_api_log_display_entries;
+
+			$db = new WP_REST_API_Log_DB_Entries();
+
+			$this->enqueue_scripts();
+
+			$data = array(
+				'nonce'   => wp_create_nonce( 'wp_rest' ),
+				'route'   => rest_url( '/wp-rest-api-log/entries' ),
+				'id'      => absint( filter_input( INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT ) ),
+				);
+
+			$entries = $db->search( array( 'id' => $data['id'] ) );
+
+			wp_localize_script( $this->plugin_name(), 'wp_rest_api_log_admin', $data );
+
+			if ( ! empty( $entries ) && ! empty( $entries->paged_records) ) {
+				$wp_rest_api_log_display_entries = $entries->paged_records;
+			}
+
+			require_once dirname( __FILE__ ) . '/partials/wp-rest-api-log-display-entries.php';
+
+		}
+
+
+		public function entries_to_html( $entries ) {
+
+			global $wp_rest_api_log_display_entries;
+			$wp_rest_api_log_display_entries = $entries;
+
+			ob_start();
+			require_once plugin_dir_path( __FILE__ ) . 'partials/wp-rest-api-log-display-entries-table.php';
+			$html = ob_get_contents();
+			ob_end_clean();
+
+			return $html;
+		}
+
+
+		private function enqueue_scripts() {
+
+			wp_enqueue_script( 'jquery' );
+			wp_enqueue_script( 'jquery-ui-datepicker' );
+
+			// https://highlightjs.org/
+			wp_enqueue_script( 'highlight-js', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js' );
+			wp_enqueue_style( 'highlight-js', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/styles/default.min.css' );
+
+			// http://trentrichardson.com/examples/timepicker/
+			wp_enqueue_script( 'jquery-ui-timepicker', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.4.5/jquery-ui-timepicker-addon.min.js' );
+			wp_enqueue_style( 'jquery-ui-timepicker', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.4.5/jquery-ui-timepicker-addon.min.css' );
+
+			wp_enqueue_script( $this->plugin_name(), plugin_dir_url( __FILE__ ) . 'js/wp-rest-api-log-admin.js', 'jquery', WP_REST_API_Log_Common::$version );
+
+			wp_enqueue_style( 'jquery-ui-datepicker', 'https://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css' );
+			wp_enqueue_style( $this->plugin_name(), plugin_dir_url( __FILE__ ) . 'css/wp-rest-api-log-admin.css', '', WP_REST_API_Log_Common::$version );
+
+
+		}
+
+
+		private function plugin_name() {
+			return WP_REST_API_Log_Common::$plugin_name . '-admin';
+		}
+
+
+
+	}
+
+}
---
+++ includes/class-wp-rest-api-log-controller.php
@@ -0,0 +1,180 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) die( 'restricted access' );
+
+if ( ! class_exists( 'WP_REST_API_Log_Controller' ) ) {
+
+	class WP_REST_API_Log_Controller {
+
+		static $namespace     = 'wp-rest-api-log';
+
+		public function plugins_loaded() {
+			add_action( 'rest_api_init', array( $this, 'register_rest_routes' ) );
+		}
+
+
+		public function register_rest_routes() {
+
+			register_rest_route( self::$namespace, '/entries', array(
+				'methods'             => array( WP_REST_Server::READABLE ),
+				'callback'            => array( $this, 'get_items' ),
+				'permission_callback' => array( $this, 'get_permissions_check' ),
+				'args'                => array(
+					'from'            => array(
+						'default'           => '',
+						),
+					'to'                    => array(
+						'default'              => current_time( 'mysql' ),
+						),
+					'fields'                => array(
+						'default'              => 'basic',
+						),
+					'route'                 => array(
+						'default'              => '',
+						),
+					'route-match-type'      => array(
+						'sanitize_callback'    => 'sanitize_key',
+						'default'              => 'wildcard',
+						),
+					'id'                    => array(
+						'sanitize_callback'    => 'absint',
+						'default'              => 0,
+						),
+					'after-id'              => array(
+						'sanitize_callback'    => 'absint',
+						'default'              => 0,
+						),
+					'before-id'             => array(
+						'sanitize_callback'    => 'absint',
+						'default'              => 0,
+						),
+					'page'                  => array(
+						'sanitize_callback'    => 'absint',
+						'default'              => 1,
+						),
+					'records-per-page'      => array(
+						'sanitize_callback'    => 'absint',
+						'default'              => 20,
+						),
+					'response_type'         => array(
+						'default'           => 'json',
+						),
+					'params'                => array(
+						),
+				),
+			) );
+
+
+			register_rest_route( self::$namespace, '/entries/(?P<id>[\d]+)', array(
+				'methods'             => array( WP_REST_Server::READABLE ),
+				'callback'            => array( $this, 'get_item' ),
+				'permission_callback' => array( $this, 'get_permissions_check' ),
+				'args'                => array(
+					'fields'                => array(
+						'default'              => 'basic',
+					),
+					'id'                    => array(
+						'sanitize_callback'    => 'absint',
+						'default'              => 0,
+					),
+				),
+			) );
+
+
+			register_rest_route( self::$namespace, '/entries', array(
+				'methods'             => array( WP_REST_Server::DELETABLE ),
+				'callback'            => array( $this, 'delete_items' ),
+				'permission_callback' => array( $this, 'delete_items_permissions_check' ),
+				'args'                => array(
+					'older-than-seconds'       => array(
+						'sanitize_callback'    => 'absint',
+						'default'              => DAY_IN_SECONDS * 30,
+					),
+				),
+			) );
+
+
+			register_rest_route( self::$namespace, '/routes', array(
+				'methods'             => array( WP_REST_Server::READABLE ),
+				'callback'            => array( $this, 'get_routes' ),
+				'permission_callback' => array( $this, 'get_permissions_check' ),
+
+			) );
+
+		}
+
+
+		public function get_items( WP_REST_Request $request ) {
+			$args = array(
+				'id'                  => $request['id'],
+				'fields'              => $request['fields'],
+				'page'                => $request['page'],
+				'records_per_page'    => $request['records-per-page'],
+				'after_id'            => $request['after-id'],
+				'before_id'           => $request['before-id'],
+				'from'                => $request['from'],
+				'to'                  => $request['to'],
+				'method'              => $request['method'],
+				'route'               => $request['route'],
+				'route_match_type'    => $request['route-match-type'],
+				'params'              => $request['params'],
+				);
+
+			$db = new WP_REST_API_Log_DB_Entries();
+			$db_response = $db->search( $args );
+
+			$api_response = new WP_REST_API_Log_Entries_Response( $db_response );
+
+			if ( 'wp_admin_html' === $request['response_type'] && ! empty( $api_response->paged_records ) ) {
+				$admin = new WP_REST_API_Log_Admin();
+				$api_response->entries_html = $admin->entries_to_html( $api_response->paged_records );
+				$api_response->paged_records = array(); // no need to send this data back to the client
+			}
+
+			return rest_ensure_response( $api_response );
+
+		}
+
+
+		public function get_item( WP_REST_Request $request ) {
+			$args = array(
+				'id'                  => $request['id'],
+				'fields'              => $request['fields'],
+				);
+
+			$db = new WP_REST_API_Log_DB_Entries();
+			return rest_ensure_response( new WP_REST_API_Log_Entries_Response( $db->search( $args ) ) );
+
+		}
+
+
+		public function get_routes( WP_REST_Request $request ) {
+
+			$db = new WP_REST_API_Log_DB_Entries();
+			return rest_ensure_response( new WP_REST_API_Log_Routes_Response( $db->distinct_routes() ) );
+
+		}
+
+
+		public function delete_items( WP_REST_Request $request ) {
+			$args = array(
+				'older_than_seconds'  => $request['older-than-seconds'],
+				);
+
+			$db = new WP_REST_API_Log_DB_Entries();
+			return rest_ensure_response( new WP_REST_API_Log_Delete_Response( $db->delete( $args ) ) );
+		}
+
+
+		public function get_permissions_check() {
+			return apply_filters( WP_REST_API_Log_Common::$plugin_name . '-can-view-entries', current_user_can( 'manage_options' ) );
+		}
+
+
+		public function delete_items_permissions_check() {
+			return apply_filters( WP_REST_API_Log_Common::$plugin_name . '-can-delete-entries', current_user_can( 'manage_options' ) );
+		}
+
+	}
+
+}
